02aec4ae5f56c1c4792db0ce23d2286b3bdbd28c
adi <nikumbh.adi@gmail.com>
2021-12-01 23:43:38 -0800

    Day 2 start

--- Day 1/day1part1.py
+++ Day 1/day1part1.py
@@ -1,32 +1,11 @@
-int_text = 0
+n = 0 
 result = 0
-n=0
-
 fileContents = open("day1index.txt")
 stringed_txt = fileContents.read().split('\n')
-stringed_txt = [int(i) for i in stringed_txt]
+int_text = [int(i) for i in stringed_txt]
 
-int_text=stringed_txt
-
-for i in range(2, len(int_text)):
-    difference = int_text[n+1] - int_text[n]
+for i in range(1, len(int_text)):
+    difference = int_text[i] - int_text[i-1]
     if difference > 0:
         result +=1
-        n +=1
-    if difference < 0:
-        n +=1
 print(result)
-
-result2 = 0 
-
-firstint = int_text[0]
-secondint = int_text[1]
-thirdint = int_text[2]
-sum_block_1 = firstint + secondint + thirdint
-
-
-for i in range(2, len(int_text)):
-    difference2 = (sum_block_1 - int_text[i-3]) + int_text[i]
-    if difference2 > sum_block_1:
-        result2 +=1
-print(result2)
