129fd2671eeb02feb7bd27b35097fbb04c250083
adi <nikumbh.adi@gmail.com>
2021-12-08 02:24:57 -0800

    7?

---
+++ day3solution.py
@@ -0,0 +1,20 @@
+import numpy
+
+
+
+
+# fileContents = open("day3index.txt")
+# stringed_txt = fileContents.read().split('\n')
+# int_text = [int(i) for i in stringed_txt]
+# n=-1
+# def get_digit(number, n):
+#     return number // 10**n % 10
+# for i in range(1, len(int_text)):
+#     n +=1
+#     digit_1 = get_digit(int_text[n], 0)
+#     digit_2 = get_digit(int_text[n+1], 0)
+   
+#     print(digit_1 + digit_2)
+
+
+
